e37c539f4439a2b3a534089f8bb7b0e7fc2fd09c
kyle misener <km.pacific.27@gmail.com>
2015-09-25 16:45:11 -0700

    added flipCard method to check cards if matched

--- assets/js/tiles.js
+++ assets/js/tiles.js
@@ -8,59 +8,69 @@ var memory = (function() {
 
   var arrays = {
 
-          linux: ['img/linux/linuxDebian', 'img/linux/linuxDebian',
-              'img/linux/linuxArch', 'img/linux/linuxArch',
-              'img/linux/linuxUbuntu', 'img/linux/linuxUbuntu',
-              'img/linux/linuxOpenSuse', 'img/linux/linuxOpenSuse',
-              'img/linux/linuxChakra', 'img/linux/linuxChakra',
-              'img/linux/linuxSliTaz', 'img/linux/linuxSliTaz',
-              'img/linux/linuxAntiX', 'img/linux/linuxAntiX',
-              'img/linux/linuxMint', 'img/linux/linuxMint',
-              'img/linux/linuxKali', 'img/linux/linuxKali',
-              'img/linux/linuxSlackWare', 'img/linux/linuxSlackWare',
-              'img/linux/linuxGentoo', 'img/linux/linuxGentoo',
-              'img/linux/linuxPuppy', 'img/linux/linuxPuppy',
-              'img/linux/linuxBodhi', 'img/linux/linuxBodhi',
-              'img/linux/linuxCentOS', 'img/linux/linuxCentOS',
-              'img/linux/linuxElementary', 'img/linux/linuxElementary'
+          linux: ['assets/img/linux/linuxDebian', 'assets/img/linux/linuxDebian',
+              'assets/img/linux/linuxArch', 'assets/img/linux/linuxArch',
+              'assets/img/linux/linuxUbuntu', 'assets/img/linux/linuxUbuntu',
+              'assets/img/linux/linuxOpenSuse', 'assets/img/linux/linuxOpenSuse',
+              'assets/img/linux/linuxNetrunner', 'assets/img/linux/linuxNetrunner',
+              'assets/img/linux/linuxSliTaz', 'assets/img/linux/linuxSliTaz',
+              'assets/img/linux/linuxAntiX', 'assets/img/linux/linuxAntiX',
+              'assets/img/linux/linuxMint', 'assets/img/linux/linuxMint',
+              'assets/img/linux/linuxKali', 'assets/img/linux/linuxKali',
+              'assets/img/linux/linuxFreeBsd', 'assets/img/linux/linuxFreeBsd',
+              'assets/img/linux/linuxGentoo', 'assets/img/linux/linuxGentoo',
+              'assets/img/linux/linuxRedHat', 'assets/img/linux/linuxRedHat',
+              'assets/img/linux/linuxBodhi', 'assets/img/linux/linuxBodhi',
+              'assets/img/linux/linuxCentOS', 'assets/img/linux/linuxCentOS',
+              'assets/img/linux/linuxElementary', 'assets/img/linux/linuxElementary'
           ],
 
-          seinfeld: ['img/seinfeld/seinfeldBoss', 'img/seinfeld/seinfeldBoss',
-              'img/seinfeld/seinfeldCastanzaMom', 'img/seinfeld/seinfeldCastanzaMom',
-              'img/seinfeld/seinfeldCastanza', 'img/seinfeld/seinfeldCastanza',
-              'img/seinfeld/seinfeldCosmo', 'img/seinfeld/seinfeldCosmo',
-              'img/seinfeld/seinfeldElaine', 'img/seinfeld/seinfeldElaine',
-              'img/seinfeld/seinfeldGeorge', 'img/seinfeld/seinfeldGeorge',
-              'img/seinfeld/seinfeldGroup', 'img/seinfeld/seinfeldGroup',
-              'img/seinfeld/seinfeldJerry', 'img/seinfeld/seinfeldJerry',
-              'img/seinfeld/seinfeldNewman', 'img/seinfeld/seinfeldNewman',
-              'img/seinfeld/seinfeldNewman2', 'img/seinfeld/seinfeldNewman2',
-              'img/seinfeld/seinfeldSoupNazi', 'img/seinfeld/seinfeldSoupNazi',
-              'img/seinfeld/seinfeldSteinbrenner', 'img/seinfeld/seinfeldSteinbrenner',
-              'img/seinfeld/seinfeldUncleLeo', 'img/seinfeld/seinfeldUncleLeo',
-              'img/seinfeld/seinfeldDecider', 'img/seinfeld/seinfeldDecider',
-              'img/seinfeld/seinfeldBabu', 'img/seinfeld/seinfeldBabu'
+          seinfeld: ['assets/img/seinfeld/seinfeldBoss', 'assets/img/seinfeld/seinfeldBoss',
+              'assets/img/seinfeld/seinfeldCastanzaMom', 'assets/img/seinfeld/seinfeldCastanzaMom',
+              'assets/img/seinfeld/seinfeldCastanza', 'assets/img/seinfeld/seinfeldCastanza',
+              'assets/img/seinfeld/seinfeldCosmo', 'assets/img/seinfeld/seinfeldCosmo',
+              'assets/img/seinfeld/seinfeldElaine', 'assets/img/seinfeld/seinfeldElaine',
+              'assets/img/seinfeld/seinfeldGeorge', 'assets/img/seinfeld/seinfeldGeorge',
+              'assets/img/seinfeld/seinfeldGroup', 'assets/img/seinfeld/seinfeldGroup',
+              'assets/img/seinfeld/seinfeldJerry', 'assets/img/seinfeld/seinfeldJerry',
+              'assets/img/seinfeld/seinfeldNewman', 'assets/img/seinfeld/seinfeldNewman',
+              'assets/img/seinfeld/seinfeldNewman2', 'assets/img/seinfeld/seinfeldNewman2',
+              'assets/img/seinfeld/seinfeldSoupNazi', 'assets/img/seinfeld/seinfeldSoupNazi',
+              'assets/img/seinfeld/seinfeldSteinbrenner', 'assets/img/seinfeld/seinfeldSteinbrenner',
+              'assets/img/seinfeld/seinfeldUncleLeo', 'assets/img/seinfeld/seinfeldUncleLeo',
+              'assets/img/seinfeld/seinfeldDecider', 'assets/img/seinfeld/seinfeldDecider',
+              'assets/img/seinfeld/seinfeldBabu', 'assets/img/seinfeld/seinfeldBabu'
           ],
 
-          starTrek: ['img/starTrek/starTrekWorf', 'img/starTrek/starTrekWorf',
-              'img/starTrek/starTrekCisco', 'img/starTrek/starTrekCisco',
-              'img/starTrek/starTrekData', 'img/starTrek/starTrekData',
-              'img/starTrek/starTrekKirk', 'img/starTrek/starTrekKirk',
-              'img/starTrek/starTrekSpock', 'img/starTrek/starTrekSpock',
-              'img/starTrek/starTrekQuark', 'img/starTrek/starTrekQuark',
-              'img/starTrek/starTrekMartok', 'img/starTrek/starTrekMartok',
-              'img/starTrek/starTrekGarok', 'img/starTrek/starTrekGarok',
-              'img/starTrek/starTrekDax', 'img/starTrek/starTrekDax',
-              'img/starTrek/starTrekGeordi', 'img/starTrek/starTrekGeordi',
-              'img/starTrek/starTrekKhan', 'img/starTrek/starTrekKhan',
-              'img/starTrek/starTrekOdo', 'img/starTrek/starTrekOdo',
-              'img/starTrek/starTrekBorgQueen', 'img/starTrek/starTrekBorgQueen',
-              'img/starTrek/starTrekGalDukat', 'img/starTrek/starTrekGalDukat',
-              'img/starTrek/starTrekSevenOfNine', 'img/starTrek/starTrekSevenOfNine'
+          starTrek: ['assets/img/starTrek/starTrekWorf', 'assets/img/starTrek/starTrekWorf',
+              'assets/img/starTrek/starTrekCisco', 'assets/img/starTrek/starTrekCisco',
+              'assets/img/starTrek/starTrekData', 'assets/img/starTrek/starTrekData',
+              'assets/img/starTrek/starTrekKirk', 'assets/img/starTrek/starTrekKirk',
+              'assets/img/starTrek/starTrekSpock', 'assets/img/starTrek/starTrekSpock',
+              'assets/img/starTrek/starTrekQuark', 'assets/img/starTrek/starTrekQuark',
+              'assets/img/starTrek/starTrekMartok', 'assets/img/starTrek/starTrekMartok',
+              'assets/img/starTrek/starTrekGarok', 'assets/img/starTrek/starTrekGarok',
+              'assets/img/starTrek/starTrekDax', 'assets/img/starTrek/starTrekDax',
+              'assets/img/starTrek/starTrekGeordi', 'assets/img/starTrek/starTrekGeordi',
+              'assets/img/starTrek/starTrekKhan', 'assets/img/starTrek/starTrekKhan',
+              'assets/img/starTrek/starTrekOdo', 'assets/img/starTrek/starTrekOdo',
+              'assets/img/starTrek/starTrekBorgQueen', 'assets/img/starTrek/starTrekBorgQueen',
+              'assets/img/starTrek/starTrekGalDukat', 'assets/img/starTrek/starTrekGalDukat',
+              'assets/img/starTrek/starTrekSevenOfNine', 'assets/img/starTrek/starTrekSevenOfNine'
           ]
       };//end arrays
 
       /*Global Variables*/
+      var card_matched = [];
+      var memory_values = [];
+      var memory_card_id = [];
+      var card_matched = [];
+      var card_previous = [];
+      var cards_flipped = 0;
+      var userAttempt = 0;
+      var cards_flipped = 0;
+      var gamesPlayed = 0;
+      var userSelect = "";
       var deck = "";
       var cardBackGround = "";
 
@@ -132,8 +142,88 @@ var memory = (function() {
               changeBackground();
           } //End Shuffle
 
+        function flipCard(card, val) {
+
+                //get passed in cards id
+                var userSelect = card.id;
+
+                /*check to see if passed in card has been previously matched if so alert user to select different card.*/
+                if ($.inArray(userSelect, card_matched) == -1 && $.inArray(userSelect, card_previous) == -1) {
+
+                    if (card.innerHTML == "" && memory_values.length < 2) {
+
+                        card.style.background = 'url(' + val + '.jpg) no-repeat';
+                        card.innerHTML = "";
+
+                        /*If this is the first card clicked push card value onto the memory_values(stack)
+    		                 *and push card.id onto memory_card_id(stack)*/
+                        if (memory_values.length == 0) {
+                            memory_values.push(val);
+                            memory_card_id.push(card.id);
+    			                  card_previous.push(card.id);
+                            userAttempt++;
+                        } //End if
+
+                        /*if this is the second card clicked push card value onto the memory_values(stack)
+    		                 *and push card.id onto memory_card_id(stack)*/
+                        else if (memory_values.length == 1) {
+                            memory_values.push(val);
+                            memory_card_id.push(card.id);
+                            userAttempt++;
+
+                            /*Compare card values on the stack after two have been clicked to see if they are the same card.*/
+                            if (memory_values[0] == memory_values[1]) {
+                                cards_flipped += 2;
+
+                                /*Add cards matched to array used to safeguard against user picking cards previously matched*/
+                                var temp1 = memory_card_id.pop();
+                                var temp2 = memory_card_id.pop();
+                                card_matched.push(temp1);
+                                card_matched.push(temp2);
+                                temp1 = "";
+                                temp2 = "";
+
+                                /*Clear out arrays/stack because cards were a match to get array ready for next user selection*/
+                                memory_values = [];
+                                memory_card_id = [];
+
+                                /*Check to see if all cards have been flipped if so alert user and generate new board*/
+                                if (cards_flipped == arrays[deck].length) {
+    				                        //stop timer & calculate attempts call get player score and pass (name, time , attempts)
+                                    alert("Congratulations!! This round took " + userAttempt / 2 + " attempts");
+                                    document.getElementById('memory-board').innerHTML = "";
+    				                        card_previous = [];
+                                    userAttempt = 0;
+                                    card_matched = [];
+                                    shuffle();
+                                } //End if
+                            } //End if
+                            else {
+                                //If cards were not a match flip them back over
+                                function flipCardBack() {
+                                        var card_1 = document.getElementById(memory_card_id[0]);
+                                        var card_2 = document.getElementById(memory_card_id[1]);
+                                        card_1.style.background = 'url(' + cardBackGround + ') no-repeat';
+                                        card_1.innerHTML = "";
+                                        card_2.style.background = 'url(' + cardBackGround + ') no-repeat';
+                                        card_2.innerHTML = "";
+    				                            card_previous = [];
+                                        memory_values = [];
+                                        memory_card_id = [];
+                                    } //End flipCardBack
+                                setTimeout(flipCardBack, 700);
+                            } //End else
+                        } //End else if
+                    } //End if
+                } //End if
+                else {
+                    alert("This card has previously been selected, please choose a different card.");
+                } //End else
+            } //End flipCard
+
       var public = {
           shuffle: shuffle,
+          flipCard: flipCard,
           arrays: arrays,
           deckSelected: deckSelected,
           changeBackground: changeBackground
